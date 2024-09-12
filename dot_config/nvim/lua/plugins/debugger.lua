f3e5f90448f7b95e42f9eafdf45b3c000a886f55
willheryanto <will.heryanto@gmail.com>
2024-09-12 20:32:11 +0700

    feat: udpate

--- dot_config/nvim/lua/plugins/debugger.lua
+++ dot_config/nvim/lua/plugins/debugger.lua
@@ -1,35 +1,106 @@
 return {
-  {
-    "mfussenegger/nvim-dap",
-    keys = {
-      {
-        "<F5>",
-        function()
-          require("dap").continue()
-        end,
-        desc = "Continue",
-      },
-      {
-        "<F10>",
-        function()
-          require("dap").step_over()
-        end,
-        desc = "Step Over",
-      },
-      {
-        "<F11>",
-        function()
-          require("dap").step_into()
-        end,
-        desc = "Step Into",
-      },
-      {
-        "<F12>",
-        function()
-          require("dap").step_out()
-        end,
-        desc = "Step Out",
-      },
+  "mfussenegger/nvim-dap",
+  keys = {
+    {
+      "<F5>",
+      function()
+        require("dap").continue()
+      end,
+      desc = "Continue",
+    },
+    {
+      "<F10>",
+      function()
+        require("dap").step_over()
+      end,
+      desc = "Step Over",
+    },
+    {
+      "<F11>",
+      function()
+        require("dap").step_into()
+      end,
+      desc = "Step Into",
+    },
+    {
+      "<F12>",
+      function()
+        require("dap").step_out()
+      end,
+      desc = "Step Out",
     },
   },
+  opts = function()
+    local languages = { "typescript", "javascript", "typescriptreact", "javascriptreact" }
+
+    local dap = require("dap")
+
+    dap.adapters["pwa-node"] = {
+      type = "server",
+      host = "localhost",
+      port = "${port}",
+      executable = {
+        command = "js-debug-adapter",
+        args = { "${port}" },
+      },
+    }
+
+    dap.adapters["pwa-chrome"] = {
+      type = "server",
+      host = "localhost",
+      port = "${port}",
+      executable = {
+        command = "js-debug-adapter",
+        args = { "${port}" },
+      },
+    }
+
+    require("dap.ext.vscode").load_launchjs(nil, {
+      ["pwa-node"] = languages,
+      ["node"] = languages,
+      ["chrome"] = languages,
+      ["pwa-chrome"] = languages,
+    })
+
+    for _, language in ipairs(languages) do
+      vim.list_extend(dap.configurations[language], {
+        {
+          type = "pwa-node",
+          request = "launch",
+          name = "My launch file",
+          cwd = "${workspaceFolder}",
+          runtimeExecutable = "ts-node",
+          args = { "${file}" },
+          sourceMaps = true,
+          protocol = "inspector",
+          skipFiles = { "<node_internals>/**", "node_modules/**" },
+          resolveSourceMapLocations = {
+            "${workspaceFolder}/**",
+            "!**/node_modules/**",
+          },
+        },
+        {
+          type = "pwa-node",
+          request = "attach",
+          name = "My attach to process",
+          processId = require("dap.utils").pick_process,
+          cwd = "${workspaceFolder}",
+        },
+        {
+          type = "pwa-chrome",
+          request = "launch",
+          name = "My start Chrome on port",
+          url = function()
+            local port = vim.fn.input({
+              prompt = "Port ",
+              completion = "file",
+            })
+            return "http://localhost:3000" .. port
+          end,
+          webRoot = "${workspaceFolder}",
+          userDataDir = "${workspaceFolder}/.vscode/vscode-chrome-debug-userdatadir",
+        },
+      })
+    end
+  end,
 }
--- dot_config/nvim/lua/plugins/test.lua
+++ dot_config/nvim/lua/plugins/test.lua
@@ -7,12 +7,19 @@ return {
     opts = function(_, opts)
       opts.adapters = vim.tbl_extend("force", opts.adapters, {
         require("neotest-jest")({
-          -- jestCommand = "npm run test:single -- --selectProjects product-transactions",
-          jestCommand = "node --expose-gc --no-compilation-cache ./node_modules/jest/bin/jest.js --logHeapUsage --colors --silent",
+          -- jestCommand = "node --expose-gc --no-compilation-cache ./node_modules/jest/bin/jest.js --logHeapUsage --colors --silent",
+          jestCommand = "npm run jest",
           jestConfigFile = "jest.config.js",
           env = { CI = true },
           cwd = function(path)
             return vim.fn.getcwd()
+          end,
+          strategy_config = function(default_strategy, _)
+            default_strategy["resolveSourceMapLocations"] = {
+              "${workspaceFolder}/**",
+              "!**/node_modules/**",
+            }
+            return default_strategy
           end,
         }),
       })
