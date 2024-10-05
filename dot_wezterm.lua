017820b1215d172f5b62ba8cb15743ed28136a78
willheryanto <will.heryanto@gmail.com>
2024-10-05 13:29:25 +0700

    feat: update

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -14,7 +14,35 @@ end
 
 -- For example, changing the color scheme:
 -- config.color_scheme = "Tokyo Night Moon"
-config.color_scheme = "Catppuccin Mocha"
+-- config.color_scheme = "Catppuccin Mocha"
+config.colors = {
+	foreground = "#f9f6ee",
+	background = "#090a04",
+	cursor_bg = "#c1c1c1",
+	cursor_fg = "#090a04",
+	selection_fg = "#f9f6ee",
+	selection_bg = "#3c3836",
+	ansi = {
+		"#1d2021",
+		"#ff3131",
+		"#98971a",
+		"#d79921",
+		"#458588",
+		"#b16286",
+		"#689d6a",
+		"#a89984",
+	},
+	brights = {
+		"#928374",
+		"#fb4934",
+		"#b8bb26",
+		"#fabd2f",
+		"#83a598",
+		"#d3869b",
+		"#8ec07c",
+		"#ebdbb2",
+	},
+}
 
 config.font = wezterm.font("MonoLisa")
 config.font_size = 13.0
