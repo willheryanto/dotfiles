c34c3d2cc6e640cf8df31e30ef98f3f708a13ffa
willheryanto <will.heryanto@gmail.com>
2025-03-14 14:42:32 +0700

    feat: update

--- dot_config/nvim/lua/plugins/colorschema.lua
+++ dot_config/nvim/lua/plugins/colorschema.lua
@@ -8,5 +8,17 @@ return {
       -- require("night-owl").setup()
       -- vim.cmd.colorscheme("night-owl")
     end,
-  }
+  },
+  {
+    "rebelot/kanagawa.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      -- theme = "dragon",
+    },
+    config = function(_, opts)
+      require("kanagawa").setup(opts)
+      vim.cmd.colorscheme("kanagawa-dragon")
+    end,
+  },
 }
