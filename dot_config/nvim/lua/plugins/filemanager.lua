0deb6b15292ae06651a381d9dcbf747b1bd39053
willheryanto <will.heryanto@gmail.com>
2025-04-28 11:56:04 +0700

    feat: update

--- dot_config/nvim/lua/plugins/filemanager.lua
+++ dot_config/nvim/lua/plugins/filemanager.lua
@@ -15,6 +15,13 @@ return {
       win_options = {
         signcolumn = "yes:2",
       },
+      view_options = {
+        show_hidden = true,
+      },
+      keymaps = {
+        ["<C-r>"] = "actions.refresh", -- Bind refresh to Ctrl+R
+        ["<C-l>"] = false,             -- Unbind refresh from Ctrl+L
+      },
     },
     dependencies = { { "echasnovski/mini.icons", opts = {} } },
   },
