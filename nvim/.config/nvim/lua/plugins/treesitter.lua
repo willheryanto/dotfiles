7126d1e1dc35e6ab9e2f3b69c04484c2410555e8
willheryanto <will.heryanto@gmail.com>
2026-04-28 20:24:56 +0700

    nvim: add treesitter textobject moves for functions and classes

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -15,7 +15,7 @@ return {
 			"vimdoc",
 			"query",
 			"regex",
-      "bash"
+			"bash",
 		},
 	},
 	config = function(_, opts)
@@ -44,15 +44,49 @@ return {
 			select = {
 				lookahead = true,
 			},
+			move = {
+				set_jumps = true,
+			},
 		})
 
 		local ts_select = require("nvim-treesitter-textobjects.select")
+		local ts_move = require("nvim-treesitter-textobjects.move")
 		vim.keymap.set({ "x", "o" }, "ic", function()
 			ts_select.select_textobject("@block.inner", "textobjects")
 		end, { desc = "TS Textobject: inner block" })
 		vim.keymap.set({ "x", "o" }, "ac", function()
 			ts_select.select_textobject("@block.outer", "textobjects")
 		end, { desc = "TS Textobject: outer block" })
+		vim.keymap.set({ "x", "o" }, "if", function()
+			ts_select.select_textobject("@function.inner", "textobjects")
+		end, { desc = "TS Textobject: inner function" })
+		vim.keymap.set({ "x", "o" }, "af", function()
+			ts_select.select_textobject("@function.outer", "textobjects")
+		end, { desc = "TS Textobject: outer function" })
+		vim.keymap.set({ "n", "x", "o" }, "]f", function()
+			ts_move.goto_next_start("@function.outer", "textobjects")
+		end, { desc = "TS Textobject: next function start" })
+		vim.keymap.set({ "n", "x", "o" }, "]F", function()
+			ts_move.goto_next_end("@function.outer", "textobjects")
+		end, { desc = "TS Textobject: next function end" })
+		vim.keymap.set({ "n", "x", "o" }, "[f", function()
+			ts_move.goto_previous_start("@function.outer", "textobjects")
+		end, { desc = "TS Textobject: previous function start" })
+		vim.keymap.set({ "n", "x", "o" }, "[F", function()
+			ts_move.goto_previous_end("@function.outer", "textobjects")
+		end, { desc = "TS Textobject: previous function end" })
+		vim.keymap.set({ "n", "x", "o" }, "]c", function()
+			ts_move.goto_next_start("@class.outer", "textobjects")
+		end, { desc = "TS Textobject: next class start" })
+		vim.keymap.set({ "n", "x", "o" }, "]C", function()
+			ts_move.goto_next_end("@class.outer", "textobjects")
+		end, { desc = "TS Textobject: next class end" })
+		vim.keymap.set({ "n", "x", "o" }, "[c", function()
+			ts_move.goto_previous_start("@class.outer", "textobjects")
+		end, { desc = "TS Textobject: previous class start" })
+		vim.keymap.set({ "n", "x", "o" }, "[C", function()
+			ts_move.goto_previous_end("@class.outer", "textobjects")
+		end, { desc = "TS Textobject: previous class end" })
 
 		-- Enable treesitter highlighting for all filetypes
 		vim.api.nvim_create_autocmd("FileType", {
